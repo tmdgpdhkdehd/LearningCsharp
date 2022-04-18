7ef926366796ac08b231f4f92ec98c72a33d34db
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-04-19 00:50:50 +0900

    Create PictureBox.cs

---
+++ Forms/PictureBox.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formsPictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            bmp.Save("output.png", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
